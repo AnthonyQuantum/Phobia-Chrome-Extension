26c50572638dafa9eb5fca2bbb9f1f6dced208ca
AnthonyQuantum <quantumcreeper12@gmail.com>
2018-04-25 11:36:40 +0300

    4/25/2018 11:36AM

--- background.js
+++ background.js
@@ -1,14 +1,27 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+    chrome.storage.sync.set({enabled: true}, function() {});
+    chrome.storage.sync.set({strictEnabled: false}, function() {});
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.google.ru'},
+        pageUrl: {schemes: ['https', 'http']},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
+
+  chrome.webNavigation.onCompleted.addListener(function() {
+    chrome.storage.sync.get('enabled', function(data) {
+      if (data.enabled)
+      {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.executeScript(
+              tabs[0].id,
+              {code: 'const images = document.getElementsByTagName("img"); for (let image of images) { image.src = "http://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg"; }'});
+        });
+      }
+  });
+})
+  
--- popup.js
+++ popup.js
@@ -29,8 +29,28 @@
   }
   constructFilters(titles);
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'const images = document.getElementsByTagName("img"); for (let image of images) { image.src = "http://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg"; }'});
+    });*/
+
+    chrome.storage.sync.get('enabled', function(data) {
+        if (data.enabled)
+            document.getElementById('enabled-slider').setAttribute('checked', true);
+    });
+    chrome.storage.sync.get('strictEnabled', function(data) {
+        if (data.strictEnabled)
+            document.getElementById('strict-slider').setAttribute('checked', true);
+    });
+
+    const enabledSlider = document.getElementById('enabled-slider');
+    enabledSlider.addEventListener('click', function() {
+        chrome.storage.sync.set({enabled: !enabledSlider.getAttribute('checked')}, 
+        function() { });
+    });
+    const strictSlider = document.getElementById('strict-slider');
+    strictSlider.addEventListener('click', function() {
+        chrome.storage.sync.set({strictEnabled: !strictSlider.getAttribute('checked')}, 
+        function() { });
     });
