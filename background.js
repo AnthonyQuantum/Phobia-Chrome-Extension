daacbc5fa796c795d0f89b09bf75e32779f4c7d1
AnthonyQuantum <quantumcreeper12@gmail.com>
2018-04-25 20:45:36 +0300

    4/25/2018 8:45PM

--- background.js
+++ background.js
@@ -1,16 +1,40 @@
 chrome.webNavigation.onDOMContentLoaded.addListener(function() {
   chrome.storage.sync.get('enabled', function(data) {
-    if (data.enabled)
+    if (data.enabled && !data.strictEnabled)
     {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {code: 'const images = document.getElementsByTagName("img"); for (let image of images) { image.src = "http://www.htmlcsscolor.com/preview/16x16/CDCDCD.png"; }'});
+            {code: 'const allText = document.all[0].innerText;\
+                    if(allText.indexOf("spider") > -1) {\
+                      console.log("Blocked!");\
+                      const images = document.getElementsByTagName("img");\
+                      for (let image of images)\
+                      {\
+                        image.src = "http://www.htmlcsscolor.com/preview/16x16/CDCDCD.png"\
+                      }\
+                    }'});
       });
     }
-})
 });
 
+chrome.storage.sync.get('strictEnabled', function(data) {
+  if (data.strictEnabled)
+    {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'let html = document.getElementsByTagName("html")[0];\
+                    console.log("OK");\
+                    let warn = document.createElement("div");\
+                    warn.innerHTML = "<br><h2>Phobia</h2><br><h2>Warning! This content might be unpleasant for you.</h2>";\
+                    html.appendChild(warn);\
+                    document.getElementsByTagName("body")[0].style.display = "none";'});
+      });
+    }
+});
+
+});
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({enabled: true}, function() {});
     chrome.storage.sync.set({strictEnabled: false}, function() {});
